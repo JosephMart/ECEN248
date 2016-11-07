15427d3624aa6afcf2038cd20b09e1bf34cc9b70
Joseph Martinsen <joseph@martinsen.com>
2016-11-07 01:38:23 -0600

    PreLab 10 Done

--- Lab10/prelab/generate_propagate_unit.v
+++ Lab10/prelab/generate_propagate_unit.v
@@ -6,10 +6,10 @@ module generate_propagate_unit(G, P, X, Y);
   output wire [3:0] G, P;
   input wire [3:0] X, Y;
 
-  assign G[0] = X[0] && Y[0];
-  assign G[1] = X[1] && Y[1];
-  assign G[2] = X[2] && Y[2];
-  assign G[3] = X[3] && Y[3];
+  assign G[0] = X[0] & Y[0];
+  assign G[1] = X[1] & Y[1];
+  assign G[2] = X[2] & Y[2];
+  assign G[3] = X[3] & Y[3];
 
   assign P[0] = X[0] ^ Y[0];
   assign P[1] = X[1] ^ Y[1];
